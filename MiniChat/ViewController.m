af83517c186e81d3b344d0d8c06a0559fd60acc4
陈华谋 <chenhuamou@chenhuamoudemacbook-pro.local>
2018-04-29 16:38:52 +0800

    Initial Commit

---
+++ MiniChat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MiniChat
+//
+//  Created by 陈华谋 on 29/04/2018.
+//  Copyright © 2018 陈华谋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
