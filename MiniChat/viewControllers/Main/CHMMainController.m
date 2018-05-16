ae679328cfdc4c43a95c2785cf7cfa322a83aa9e
chenhuamou <chenhm4444@gmail.com>
2018-05-16 10:49:09 +0800

    ddd

--- MiniChat/viewControllers/Main/CHMMainController.m
+++ MiniChat/viewControllers/Main/CHMMainController.m
@@ -9,92 +9,20 @@
 #import "CHMMainController.h"
 #import "CHMInfoProvider.h"
 
-@interface CHMMainController () <RCIMReceiveMessageDelegate>
+@interface CHMMainController () 
 
 @end
 
 @implementation CHMMainController
 
-#pragma mark - RCIMReceiveMessageDelegate
-- (void)onRCIMReceiveMessage:(RCMessage *)message left:(int)left {
-    NSLog(@"-------%@",message);
-    // 好友信息
-    RCContactNotificationMessage *contactNotificationMsg = nil;
-    if ([message.objectName isEqualToString:@"RC:ContactNtf"]) {
-        contactNotificationMsg = (RCContactNotificationMessage *)message.content;
-        // 如果是同意好友申请的消息，就刷新好友列表数据
-        NSString *account = [[NSUserDefaults standardUserDefaults] valueForKey:KAccount];
-        [[CHMInfoProvider shareInstance] syncFriendList:account complete:^(NSMutableArray *friends) {
-            [[NSNotificationCenter defaultCenter] postNotificationName:KChangeUserInfoNotification object:nil];
-        }];
-    }
-    
-    // 把添加到群组的消息
-    RCGroupNotificationMessage *groupNotificationMsg = nil;
-    if ([message.objectName isEqualToString:@"RC:GrpNtf"]) {
-        groupNotificationMsg = (RCGroupNotificationMessage *)message.content;
-        // 刷新群组列表
-        [[CHMInfoProvider shareInstance] syncGroups];
-    }
-    
-}
 
 
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // 连接融云服务器
-    [self connectToRongCloud];
-    
-    [RCIM sharedRCIM].receiveMessageDelegate = self;
 }
 
-
-
-/**
- 连接融云服务器
- */
-- (void)connectToRongCloud {
-    NSString *rongCloudToken = [[NSUserDefaults standardUserDefaults] valueForKey:KRongCloudToken];
-    [[RCIM sharedRCIM] connectWithToken:rongCloudToken success:^(NSString *userId) {
-        NSLog(@"----连接成功%@",userId);
-        [self setCurrentUserInfo];
-        
-        [self refreshOfficeAccount];
-        
-    } error:^(RCConnectErrorCode status) {
-        NSLog(@"----连接失败%ld",(long)status);
-    } tokenIncorrect:^{
-        NSLog(@"----连接token不正确");
-    }];
-}
-
-
-/**
- 设置当前用户的用户信息，用于SDK显示和发送。
- */
-- (void)setCurrentUserInfo {
-    // 从沙盒中取登录时保存的用户信息
-    NSString *nickName = [[NSUserDefaults standardUserDefaults] valueForKey:KNickName];
-    NSString *account = [[NSUserDefaults standardUserDefaults] valueForKey:KAccount];
-    NSString *portrait = [[NSUserDefaults standardUserDefaults] valueForKey:KPortrait];
-    [RCIM sharedRCIM].currentUserInfo = [[RCUserInfo alloc] initWithUserId:account name:nickName portrait:portrait];
-}
-
-
-
-#pragma mark - 用户信息提供者
-
-
-
-/**
- 刷新 官方客服 和 资金助手
- */
-- (void)refreshOfficeAccount {
-    
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
